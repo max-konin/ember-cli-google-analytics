6b0bdf25c4362ef39633c7f8f0a8dfa90a168b82
Max <maxim21214@gmail.com>
2016-12-08 19:58:02 +0700

    hotfix compatibility with fastboot

--- app/mixins/google-pageview.js
+++ app/mixins/google-pageview.js
@@ -2,28 +2,32 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 
 export default Ember.Mixin.create({
+  fastboot: Ember.inject.service(); 
+  
   beforePageviewToGA: function (ga) {
 
   },
 
   pageviewToGA: Ember.on('didTransition', function(page, title) {
-    var page = page ? page : this.get('url');
-    var title = title ? title : this.get('url');
+    if !(this.get('fastboot.isFastBoot') {
+      var page = page ? page : this.get('url');
+      var title = title ? title : this.get('url');
 
-    if (Ember.get(ENV, 'googleAnalytics.webPropertyId') != null) {
-      var trackerType = Ember.getWithDefault(ENV, 'googleAnalytics.tracker', 'analytics.js');
+      if (Ember.get(ENV, 'googleAnalytics.webPropertyId') != null) {
+        var trackerType = Ember.getWithDefault(ENV, 'googleAnalytics.tracker', 'analytics.js');
 
-      if (trackerType === 'analytics.js') {
-        var globalVariable = Ember.getWithDefault(ENV, 'googleAnalytics.globalVariable', 'ga');
+        if (trackerType === 'analytics.js') {
+          var globalVariable = Ember.getWithDefault(ENV, 'googleAnalytics.globalVariable', 'ga');
 
-        this.beforePageviewToGA(window[globalVariable]);
+          this.beforePageviewToGA(window[globalVariable]);
 
-        window[globalVariable]('send', 'pageview', {
-          page: page,
-          title: title
-        });
-      } else if (trackerType === 'ga.js') {
-        window._gaq.push(['_trackPageview']);
+          window[globalVariable]('send', 'pageview', {
+            page: page,
+            title: title
+          });
+        } else if (trackerType === 'ga.js') {
+          window._gaq.push(['_trackPageview']);
+        }
       }
     }
   })
